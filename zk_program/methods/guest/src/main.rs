ed6a84a74e86e4dca9671d207c47abeeed898d06
Stanisław Durka <stas.durka@gmail.com>
2025-05-16 02:18:56 +0200

    (TEMP) comment out guest code to speed up testing flow

--- zk_program/methods/guest/src/main.rs
+++ zk_program/methods/guest/src/main.rs
@@ -31,34 +31,38 @@ fn main() {
     let raw_email_string = std::str::from_utf8(&input.raw_email).unwrap();
     println!("Raw email: {:?}", raw_email_string);
 
-    let logger = Logger::root(Discard, o!());
+    // let logger = Logger::root(Discard, o!());
 
-    let parsed_email = parse_mail(&input.raw_email).unwrap();
+    // let parsed_email = parse_mail(&input.raw_email).unwrap();
 
-    let public_key =
-        DkimPublicKey::try_from_bytes(&input.public_key, &input.public_key_type).unwrap();
+    // let public_key =
+    //     DkimPublicKey::try_from_bytes(&input.public_key, &input.public_key_type).unwrap();
 
-    let mut hasher = Sha256::new();
-    hasher.update(input.from_domain.as_bytes());
-    let from_domain_hash = hasher.finalize().to_vec();
+    // let mut hasher = Sha256::new();
+    // hasher.update(input.from_domain.as_bytes());
+    // let from_domain_hash = hasher.finalize().to_vec();
 
-    let mut hasher = Sha256::new();
-    hasher.update(&input.public_key);
-    let public_key_hash = hasher.finalize().to_vec();
+    // let mut hasher = Sha256::new();
+    // hasher.update(&input.public_key);
+    // let public_key_hash = hasher.finalize().to_vec();
 
-    let result =
-        verify_email_with_key(&logger, &input.from_domain, &parsed_email, public_key).unwrap();
+    // let result =
+    //     verify_email_with_key(&logger, &input.from_domain, &parsed_email, public_key).unwrap();
 
-    let verified = match result {
-        result if result.with_detail().starts_with("pass") => true,
-        _ => false,
-    };
+    // let verified = match result {
+    //     result if result.with_detail().starts_with("pass") => true,
+    //     _ => false,
+    // };
 
+    // let output = DKIMOutput {
+    //     from_domain_hash,
+    //     public_key_hash,
+    //     verified,
+    // };
     let output = DKIMOutput {
-        from_domain_hash,
-        public_key_hash,
-        verified,
+        from_domain_hash: vec![],
+        public_key_hash: vec![],
+        verified: false,
     };
-
     env::commit(&output);
 }
